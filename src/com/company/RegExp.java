64c59de497185d619ecfcf7fba9d0b9bcc608b92
e-number <eggnom@yandex.ru>
2021-08-14 20:45:16 +0300

    regexp Pattern Matcher

--- src/com/company/RegExp.java
+++ src/com/company/RegExp.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegExp {
     public static void main(String[] args) {
 
@@ -36,5 +40,31 @@ public class RegExp {
 
         String url = "http://www.google.com";
         System.out.println(url.matches("http://www\\..+\\.(com|ru)"));
+
+        String f = "Hell.there.hey";
+        String[] words = f.split("\\.");
+        System.out.println(Arrays.toString(words));
+
+        String g = "Hell434523there24hey";
+        String[] words2 = g.split("\\d+");
+        System.out.println(Arrays.toString(words2));
+
+        String h = "Hello there hey";
+        String modifiedString = h.replaceAll(" ", ".");
+        // replace - для замены на конкретную строку, replaceAll - для регулярных выражений
+        // replaceFirst - заменяет только первый экземпляр
+
+
+
+        String text = "Hello, Guys! I send you my email joe@gmail.com so we can\n" +
+                "keep in touch.\n" +
+                "Thanks, Joe! That's cool. I am sending you my address:\n" +
+                "tim@yandex.ru. Let's stay in touch...";
+        Pattern email = Pattern.compile("(\\w+)@(gmail|yandex)\\.(com|ru)");
+        Matcher matcher = email.matcher(text);
+
+        while(matcher.find()) {
+            System.out.println(matcher.group(1));
+        }
     }
 }
