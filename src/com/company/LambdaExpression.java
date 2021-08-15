2dd61a6d4ceafd6384f39c9bc24f19811737ad1e
e-number <eggnom@yandex.ru>
2021-08-15 23:03:48 +0300

    Lambda expression methods

--- src/com/company/LambdaExpression.java
+++ src/com/company/LambdaExpression.java
@@ -1,32 +1,71 @@
 package com.company;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class LambdaExpression {
     public static void main(String[] args) {
-        int[] arr = new int[10];
-        List<Integer> list = new ArrayList<>();
+        int[] arr1 = new int[10];
+        List<Integer> list1 = new ArrayList<>();
 
-        fillArr(arr);
-        fillList(list);
+        fillArr(arr1);
+        fillList(list1);
 
-        System.out.println(Arrays.toString(arr));
-        System.out.println(list);
+        //map method
+        arr1 = Arrays.stream(arr1).map(a -> a * 2).toArray();
+        list1 = list1.stream().map(a -> a * 2).collect(Collectors.toList());
 
-//        for (int i = 0; i < 10; i++) {
-//            arr[i] = arr[i] * 2;
-//            list.set(i, list.get(i) * 2);
-//        }
+        arr1 = Arrays.stream(arr1).map(a -> 3).toArray();
+        arr1 = Arrays.stream(arr1).map(a -> a + 1).toArray();
 
-        arr = Arrays.stream(arr).map(a -> a * 2).toArray();
-        list = list.stream().map(a -> a * 2).collect(Collectors.toList());
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(list1);
 
-        System.out.println(Arrays.toString(arr));
-        System.out.println(list);
+        //filter method
+        int[] arr2 = new int[10];
+        List<Integer> list2 = new ArrayList<>();
+
+        fillArr(arr2);
+        fillList(list2);
+
+        arr2 = Arrays.stream(arr2).filter(a -> a % 2 == 0).toArray();
+        list2 = list2.stream().filter(a -> a % 2 == 0).collect(Collectors.toList());
+
+        System.out.println(Arrays.toString(arr2));
+        System.out.println(list2);
+
+        //forEach
+        Arrays.stream(arr2).forEach(System.out::println);
+        list2.forEach(System.out::println);
+
+        //reduce
+        int[] arr3 = new int[10];
+        List<Integer> list3 = new ArrayList<>();
+
+        fillArr(arr3);
+        fillList(list3);
+
+        int sum1 = Arrays.stream(arr3).reduce((acc, b) -> acc + b).getAsInt();
+        //int sum2 = Arrays.stream(arr3).reduce(10,(acc, b) -> acc + b);
+        int mul2 = list3.stream().reduce((acc, b) -> acc * b).get();
+
+        System.out.println(sum1);
+        System.out.println(mul2);
+
+        int[] arr4 = new int[10];
+        fillArr(arr4);
+
+        int[] newArray = Arrays.stream(arr4).filter(a -> a % 2 != 0).map(a -> a * 2).toArray();
+        System.out.println(Arrays.toString(newArray));
+
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        System.out.println(set);
+        set = set.stream().map(a -> a * 3).collect(Collectors.toSet());
+        System.out.println(set);
     }
 
     private static void fillList(List<Integer> list) {
